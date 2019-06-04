d92dcbd6b666091c5903359beb45193e903175ae
githubmor <pc2man@gmail.com>
2019-06-04 15:07:33 +0430

    complete 4

--- app/src/main/java/morteza/darzi/SelfTeach/BaseDatePickerFragment.kt
+++ app/src/main/java/morteza/darzi/SelfTeach/BaseDatePickerFragment.kt
@@ -19,7 +19,7 @@ abstract class BaseDatePickerFragment : BaseFragment(), DatePickerDialog.OnDateS
                 persianCalendar.persianDay
         )
         if (selectableDateList!=null)
-            datePickerDialog.selectableDays = selectableDateList
+            datePickerDialog.highlightedDays = selectableDateList
 
         datePickerDialog.show(activity!!.fragmentManager, tag)
     }
